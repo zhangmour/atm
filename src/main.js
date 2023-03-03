539f7a6682a613e37a09e095550b3a11e2d1c2a9
hanyongyan <hanyongyan8@163.com>
2023-03-03 09:30:10 +0800

    首页

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "@/assets/css/zcx.css"
 import router from './router'
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // component: HomeView
+      component: ()=>import("@/App.vue")
     },
     {
       path: '/about',
